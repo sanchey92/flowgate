0922881f333f4de55fff9ad751a21593ebde7d38
Alexandr <sashkaageev@gmail.com>
2026-04-30 22:04:41 +0300

    added new custom errors

--- internal/domain/errors/errors.go
+++ internal/domain/errors/errors.go
@@ -2,4 +2,8 @@ package errors
 
 import "errors"
 
-var ErrNoBackends = errors.New("no backends available")
+var (
+	ErrNoBackends   = errors.New("no backends available")
+	ErrProxyStarted = errors.New("proxy already started")
+	ErrProxyStopped = errors.New("proxy already stopped")
+)
